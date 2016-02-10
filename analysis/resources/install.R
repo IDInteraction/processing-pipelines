9c607a9bf1b81fc6a15c561d38e53b8c51f8ded8
Robert Haines <rhaines@manchester.ac.uk>
2016-02-10 17:16:20 +0000

    Add analysis pipeline resources.

---
+++ analysis/resources/install.R
@@ -0,0 +1,14 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2016 The University of Manchester, UK.
+#
+# Licenced under LGPL version 2.1. See LICENCE for details.
+#
+# The IDInteraction Attention Classification was developed in the IDInteraction
+# project, funded by the Engineering and Physical Sciences Research Council,
+# UK through grant agreement number EP/M017133/1.
+#
+# Author: Robert Haines
+#------------------------------------------------------------------------------
+
+source("dataProcessing.R")
+installMissingPackages()
